11a6a0a08a8438f9242f83d53df395733965231a
Raner <r4ner2000@gmail.com>
2026-04-02 17:20:52 -0400

    feat: createSplit haptic feedback

--- app/(tabs)/add.tsx
+++ app/(tabs)/add.tsx
@@ -6,9 +6,10 @@
  * TO::DO - Integrate with backend to save new splits and manage participants.
  *******************************************************************************/
 import { supabase } from "@/lib/supabaseClient";
-
+import * as Haptics from "expo-haptics";
 import { Percent, Plus } from "lucide-react-native";
 import {
+  Alert,
   Pressable,
   ScrollView,
   StyleSheet,
@@ -71,6 +72,12 @@ export default function AddScreen() {
           },
         ],
       });
+
+      await Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
+      Alert.alert("Split Created", "Your split has been created successfully!");
+      // clear the form after success
+      setOccasionName("");
+      setTotal("");
     } catch (err) {
       console.error(err);
     }
